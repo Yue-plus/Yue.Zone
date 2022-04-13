97643aa043721fb113c88153fea8f45171e091f2
Yue-plus <Yue_plus@foxmail.com>
2022-04-13 15:45:32 +0800

    add `login.dart`

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:yue_zone/src/login/login_view.dart';
 import 'package:yue_zone/src/setting/setting_view.dart';
 
 class YueZoneApp extends StatelessWidget {
   const YueZoneApp({Key? key}) : super(key: key);
 
-  static const String title = 'YueZone - 悦域';
+  static const title = 'YueZone - 悦域';
 
-  Widget _home(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: const Text(title),
-      actions: [
-        IconButton(icon: const Icon(Icons.settings), onPressed: () {
-          Navigator.pushNamed(context, '/settings');
-        }),
-      ],
-    ),
-    body: const Center(
-      child: Text('正在生成世界……', style: TextStyle(fontSize: 32)),
-    ),
-  );
-  
+  Widget _home(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            tooltip: '设置',
+            onPressed: () {
+              Navigator.pushNamed(context, '/settings');
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.login),
+            tooltip: '登入',
+            onPressed: () {
+              Navigator.pushNamed(context, '/login');
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('正在生成世界……', style: TextStyle(fontSize: 32)),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,6 +43,7 @@ class YueZoneApp extends StatelessWidget {
       routes: {
         '/': (context) => _home(context),
         '/settings': (context) => const SettingsView(),
+        '/login': (context) => const LoginView(),
       },
       debugShowCheckedModeBanner: false,
     );
---
+++ lib/src/login/login_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  static const routeName = '/login';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('登入'),
+        ),
+        body: const Center(
+          child: SizedBox(
+            width: 600,
+            child: Text('登入', style: TextStyle(fontSize: 32)),
+          ),
+        )
+    );
+  }
+}
